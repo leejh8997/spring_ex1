490021c8245a4c7e3d4c92f617d7c26d99058574
leejh8997 <chelee24@naver.com>
2025-03-04 20:56:49 +0900

    2025.03.04 test용

--- test1/src/main/java/com/example/test1/model/Student.java
+++ test1/src/main/java/com/example/test1/model/Student.java
@@ -11,4 +11,52 @@ public class Student {
 	private String stuGender;
 	private int stuHeight;
 	private int stuWeight;
+	public String getStuNo() {
+		return stuNo;
+	}
+	public void setStuNo(String stuNo) {
+		this.stuNo = stuNo;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	public String getStuDept() {
+		return stuDept;
+	}
+	public void setStuDept(String stuDept) {
+		this.stuDept = stuDept;
+	}
+	public int getStuGrade() {
+		return stuGrade;
+	}
+	public void setStuGrade(int stuGrade) {
+		this.stuGrade = stuGrade;
+	}
+	public String getStuClass() {
+		return stuClass;
+	}
+	public void setStuClass(String stuClass) {
+		this.stuClass = stuClass;
+	}
+	public String getStuGender() {
+		return stuGender;
+	}
+	public void setStuGender(String stuGender) {
+		this.stuGender = stuGender;
+	}
+	public int getStuHeight() {
+		return stuHeight;
+	}
+	public void setStuHeight(int stuHeight) {
+		this.stuHeight = stuHeight;
+	}
+	public int getStuWeight() {
+		return stuWeight;
+	}
+	public void setStuWeight(int stuWeight) {
+		this.stuWeight = stuWeight;
+	}
 }
